f5de1dc9a52329d059df26b67b9e17d7644bc923
XDProjecta <gimosquera@uninorte.edu.co>
2025-05-24 13:40:56 -0500

    cambio pa hacer merge porque NO ME DEJA ESTA VAINA

--- src/core/controllers/updatePassengerController.java
+++ src/core/controllers/updatePassengerController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package core.controllers;
 
 import core.controllers.utils.Response;
@@ -12,10 +9,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 
-/**
- *
- * @author jmanu
- */
 public class updatePassengerController {
 
 
