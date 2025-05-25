3bf0c43ef111cd7a4bb5c3493a5948eacfcd28f0
XDProjecta <gimosquera@uninorte.edu.co>
2025-05-25 01:34:10 -0500

    passenger table controller

---
+++ src/core/controllers/Tables/PassengerTableController.java
@@ -0,0 +1,43 @@
+
+package core.controllers.Tables;
+
+import core.controllers.utils.Response;
+import core.controllers.utils.Status;
+import core.models.Passenger;
+import core.models.storage.PassengerStorage;
+import java.util.ArrayList;
+import java.util.Comparator;
+import javax.swing.table.DefaultTableModel;
+
+public class PassengerTableController {
+        public static Response updateTable(DefaultTableModel PassengerTable) {
+        try {
+            PassengerTable.setRowCount(0);
+
+            PassengerStorage storage = PassengerStorage.getInstance();
+            ArrayList<Passenger> passengers = storage.getAll();
+
+            if (passengers == null || passengers.isEmpty()) {
+                return new Response("Error: The list is empty", Status.NO_CONTENT);
+            }
+
+            passengers.sort(Comparator.comparingLong(Passenger::getId));
+
+            for (Passenger p : passengers) {
+                PassengerTable.addRow(new Object[]{
+                    p.getId(),
+                    p.getFirstname(),
+                    p.getLastname(),
+                    p.getBirthDate(),
+                    p.calculateAge(),
+                    p.generateFullPhone(),
+                    p.getCountry(),
+                    p.getNumFlights() });
+            }
+
+            return new Response("Passenger list loaded successfully", Status.OK);
+        } catch (Exception e) {
+            return new Response("Internal error: " + e.getMessage(), Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
